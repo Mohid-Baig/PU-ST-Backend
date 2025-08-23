45ca6fb7c5a3c06ffbf12ff3782f6a0debbcb5d9
mohid <mohidtahir12@gmail.com>
2025-08-23 20:52:44 +0500

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -120,6 +120,7 @@ export const loginUser = async (req, res) => {
                 id: user._id,
                 email: user.email,
                 role: user.role,
+                isVerified: user.isVerified,
             },
         });
     } catch (error) {
